e27a2b0e344312ff31c5197edd48602eb40c838d
masp9 <mart2c04@stud.kea.dk>
2022-09-27 08:42:54 +0200

    first commit

---
+++ src/main/java/com/example/gruppecicd/GruppecicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gruppecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppecicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GruppecicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gruppecicd/GruppecicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppecicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
